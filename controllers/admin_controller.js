30fd58ef9894adbee501eecea6db80d3b6a998bd
Otto Jones <ottosjonesjr@gmail.com>
2022-04-18 11:20:56 -0400

    added seat model

--- controllers/admin_controller.js
+++ controllers/admin_controller.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const admin = express.Router();
+const express = require('express')
+const admin = express.Router()
 
 // ALL FLIGHT ROUTES /////////////////////////////////
 
 // GET ROUTE FOR ALL FLIGHTS
-admin.get("/index", async (req, res) => {
+admin.get('/index', async (req, res) => {
   // const flights = await Flight.find()
   // res.status(201).json(flights)
-  res.status(201).json({ message: "Got Flights" });
-});
+  res.status(201).json({ message: 'Got Flights' })
+})
 
 // CREATE A SINGLE FLIGHT ROUTE
-admin.post("/index", async (req, res) => {
+admin.post('/index', async (req, res) => {
   //   const {
   //     flightnumber,
   //     schedule,
@@ -51,11 +51,11 @@ admin.post("/index", async (req, res) => {
   //   });
   //   res.status(201).json(newFlight);
 
-  res.status(201).json({ message: "New flight created" });
-});
+  res.status(201).json({ message: 'New flight created' })
+})
 
 // UPDATE FLIGHT ROUTE
-admin.put("/index/:id", async (req, res) => {
+admin.put('/index/:id', async (req, res) => {
   // const flight = await findById(req.params.id);
   // if (!flight) {
   //   res.status(400);
@@ -71,11 +71,11 @@ admin.put("/index/:id", async (req, res) => {
 
   res
     .status(201)
-    .json({ message: `Flight number ${req.params.id} was updated` });
-});
+    .json({ message: `Flight number ${req.params.id} was updated` })
+})
 
 // DELETE FLIGHT ROUTE
-admin.delete("/index/:id", async (req, res) => {
+admin.delete('/index/:id', async (req, res) => {
   // const flight = await Flight.findById(req.params.id);
   // if (!flight) {
   //   res.status(400);
@@ -86,38 +86,40 @@ admin.delete("/index/:id", async (req, res) => {
 
   res
     .status(201)
-    .json({ message: `Flight number ${req.params.id} was deleted` });
-});
+    .json({ message: `Flight number ${req.params.id} was deleted` })
+})
 
 // ALL SEAT ROUTES /////////////////////////////////
 
 // GET ROUTE FOR ALL SEATS THAT BELONG TO A PARTICULAR FLIGHT
-admin.get("/seats/:id", async (req, res) => {
+admin.get('/seats/:id', async (req, res) => {
   // const seatsByFlight = await Seat.find(req.params.id)
   // res.status(201).json(seatsByFlight)
   res
     .status(201)
-    .json({ message: `Got all seats that belong to flight ${req.params.id}` });
-});
+    .json({ message: `Got all seats that belong to flight ${req.params.id}` })
+})
 
 // CREATE A SEAT ROUTE
-admin.post("/seats", async (req, res) => {
-  // const { flightNumber, seatNumber, available } = req.body;
-  // if (!flightNumber || !seatNumber || !available) res.status(400);
-  // throw new Error("Please add all fields");
+admin.post('/seats', async (req, res) => {
+  // const { flightNumber, seatNumber, available, seatclass, charge } = req.body
+  // if (!flightNumber || !seatNumber || !available) res.status(400)
+  // throw new Error('Please add all fields')
   // const newSeat = await Seat.create({
   //   flightNumber,
   //   seatNumber,
   //   available,
-  //   user: req.user.id,
-  // });
-  // res.status(201).json(newSeat);
+  //   seatclass,
+  //   charge,
+  //   flightnumber: req.flight.id,
+  // })
+  // res.status(201).json(newSeat)
 
-  res.status(201).json({ message: "New seat created" });
-});
+  res.status(201).json({ message: 'New seat created' })
+})
 
 // UPDATE SEATS ROUTE
-admin.put("/seats/:id", async (req, res) => {
+admin.put('/seats/:id', async (req, res) => {
   // const seat = await Seat.findById(req.params.id);
   // if (!seat) {
   //   res.status(400);
@@ -129,10 +131,10 @@ admin.put("/seats/:id", async (req, res) => {
   // });
   // res.status(201).json(updatedSeat);
 
-  res.status(201).json({ message: `Seat number ${req.params.id} was updated` });
-});
+  res.status(201).json({ message: `Seat number ${req.params.id} was updated` })
+})
 
-admin.delete("/seats/:id", async (req, res) => {
+admin.delete('/seats/:id', async (req, res) => {
   // const seat = await Seat.findById(req.params.id);
   // if (!seat) {
   //   res.status(400);
@@ -141,6 +143,6 @@ admin.delete("/seats/:id", async (req, res) => {
   // await seat.deleteOne();
   // res.status(201).json({ id: req.params.id });
 
-  res.status(201).json({ message: `Seat number ${req.params.id} was deleted` });
-});
-module.exports = admin;
+  res.status(201).json({ message: `Seat number ${req.params.id} was deleted` })
+})
+module.exports = admin
