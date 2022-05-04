f29665f6d5f6216bbced7da602ff27d56720abdd
Otto Jones <ottosjonesjr@gmail.com>
2022-05-04 13:20:37 +0300

    working on admin routes

--- controllers/admin_controller.js
+++ controllers/admin_controller.js
@@ -3,9 +3,9 @@ const db = require('../models')
 const admin = express.Router()
 const { Flight, Passenger, Reservation, Seat } = db
 
-// ALL FLIGHT ROUTES /////////////////////////////////
+// GET ROUTE FOR ALL FLIGHTS  ////////////////////////////////
 
-// GET ROUTE FOR ALL FLIGHTS
+// GET ALL FLIGHTS ROUTE
 admin.get('/search/flights', async (req, res) => {
   try {
     const flight = await Flight.find()
@@ -16,6 +16,8 @@ admin.get('/search/flights', async (req, res) => {
     })
   }
 })
+
+// GET ALL PASSENGERS ROUTE
 admin.get('/search/passengers', async (req, res) => {
   try {
     const flight = await Passenger.find()
@@ -26,6 +28,8 @@ admin.get('/search/passengers', async (req, res) => {
     })
   }
 })
+
+// GET ALL RESERVATIOINS ROUTE
 admin.get('/search/reservations', async (req, res) => {
   try {
     const flight = await Reservation.find()
@@ -38,96 +42,105 @@ admin.get('/search/reservations', async (req, res) => {
 })
 
 // CREATE A SINGLE FLIGHT ROUTE
-admin.post('/index', async (req, res) => {
-  //   const {
-  //     flightnumber,
-  //     schedule,
-  //     departure,
-  //     destination,
-  //     departingDate,
-  //     departingTime,
-  //     arrivalDate,
-  //     arrivalTime,
-  //     totalSeatCount,
-  //   } = req.body;
+admin.post('/create-flight', async (req, res) => {
+  const {
+    flightnumber,
+    schedule,
+    departure,
+    destination,
+    departingDate,
+    departingTime,
+    arrivalDate,
+    arrivalTime,
+    totalSeatCount,
+  } = req.body
 
-  //   if (
-  //     !flightnumber ||
-  //     !schedule ||
-  //     !departure ||
-  //     !destination ||
-  //     !departingDate ||
-  //     !departingTime ||
-  //     !arrivalDate ||
-  //     !arrivalTime ||
-  //     !totalSeatCount
-  //   ) {
-  //     res.status(400);
-  //     throw new Error("Please add all fields");
-  //   }
-  //   const newFlight = await Flight.create({
-  //     flightnumber,
-  //     schedule,
-  //     departure,
-  //     destination,
-  //     departingDate,
-  //     departingTime,
-  //     arrivalDate,
-  //     arrivalTime,
-  //     totalSeatCount,
-  //   });
-  //   res.status(201).json(newFlight);
-
-  res.status(201).json({ message: 'New flight created' })
+  if (
+    !flightnumber ||
+    !schedule ||
+    !departure ||
+    !destination ||
+    !departingDate ||
+    !departingTime ||
+    !arrivalDate ||
+    !arrivalTime ||
+    !totalSeatCount
+  ) {
+    res.status(400)
+    throw new Error('Please add all fields')
+  }
+  try {
+    const newFlight = await Flight.create({
+      flightnumber,
+      schedule,
+      departure,
+      destination,
+      departingDate,
+      departingTime,
+      arrivalDate,
+      arrivalTime,
+      totalSeatCount,
+    })
+    res.status(201).json(newFlight)
+  } catch (error) {
+    res.status(400).json({
+      message: error,
+    })
+  }
 })
 
 // UPDATE FLIGHT ROUTE
-admin.post('/update-flight/:flightId', async (req, res) => {
-  const updateFlight = await findById(req.params.id).then((flight) => {
-    Seat.create(req.body)
-      .then((seat) => {
-        flight.seat.push(seat.id)
-        flight.save()
-      })
-      .catch((err) => {
-        res.status('error404')
-      })
-  })
-
-  res
-    .status(201)
-    .json({ message: `Flight number ${req.params.id} was updated` })
+admin.put('/update-flight/:flightId', async (req, res) => {
+  const flight = await findById(req.params.flightId)
+  if (!flight) {
+    res.status(400)
+    throw new Error('Flight not found')
+  }
+  try {
+    const updatedFlight = await Flight.findByIdAndUpdate(
+      req.params.flightId,
+      req.body,
+      {
+        new: true,
+      }
+    )
+    res.status(201).json(updatedFlight)
+  } catch (error) {
+    res.status(400).json({
+      message: error,
+    })
+  }
 })
 
 // DELETE FLIGHT ROUTE
-admin.delete('/index/:id', async (req, res) => {
-  // const flight = await Flight.findById(req.params.id);
-  // if (!flight) {
-  //   res.status(400);
-  //   throw new Error("Flight not found");
-  // }
-  // await flight.deleteOne();
-  // res.status(201).json({ id: req.params.id });
-
-  res
-    .status(201)
-    .json({ message: `Flight number ${req.params.id} was deleted` })
+admin.delete('/delete-flight/:flightId', async (req, res) => {
+  const flight = await Flight.findById(req.params.flightId)
+  if (!flight) {
+    res.status(400)
+    throw new Error('Flight not found')
+  }
+  try {
+    await flight.deleteOne()
+    res.status(201).json({ id: req.params.flightId })
+  } catch (error) {
+    res.status(400).json({ message: error })
+  }
 })
 
 // ALL SEAT ROUTES /////////////////////////////////
 
 // GET ROUTE FOR ALL SEATS THAT BELONG TO A PARTICULAR FLIGHT
-admin.get('/seats/:id', async (req, res) => {
-  // const seatsByFlight = await Seat.find(req.params.id)
-  // res.status(201).json(seatsByFlight)
-  res
-    .status(201)
-    .json({ message: `Got all seats that belong to flight ${req.params.id}` })
+admin.get('/seat/:seatId', async (req, res) => {
+  try {
+    const seatsByFlight = await Seat.find(req.params.id)
+    res.status(201).json(seatsByFlight)
+  } catch (error) {
+    res.status(400).json({ message: error })
+  }
 })
 
 // CREATE A SEAT ROUTE
-
-admin.post('/create/seats', async (req, res) => {
+admin.post('/create-seats', async (req, res) => {
   const { seatNumber, available, seatClass, charge } = req.body
   try {
     const seat = await Seat.create({
@@ -145,37 +158,45 @@ admin.post('/create/seats', async (req, res) => {
 })
 
 // UPDATE SEATS ROUTE
-admin.put('/seats/:id', async (req, res) => {
-  // const seat = await Seat.findById(req.params.id);
-  // if (!seat) {
-  //   res.status(400);
-  //   throw new Error("Seat not found");
-  // }
-
-  // const updatedSeat = await Seat.findByIdAndUpdate(req.params.id, req.body, {
-  //   new: true,
-  // });
-  // res.status(201).json(updatedSeat);
-
-  res.status(201).json({ message: `Seat number ${req.params.id} was updated` })
-})
-
-admin.delete('/seats/:id', async (req, res) => {
-  // const seat = await Seat.findById(req.params.id);
-  // if (!seat) {
-  //   res.status(400);
-  //   throw new Error("Seat not found");
-  // }
-  // await seat.deleteOne();
-  // res.status(201).json({ id: req.params.id });
-
-  res.status(201).json({ message: `Seat number ${req.params.id} was deleted` })
-})
-
-// REMOVE AFTER TESTING !!!!!!!!!!!
-admin.delete('/passengers', async (req, res) => {
+admin.put('/update-seats/:seatId', async (req, res) => {
+  const seat = await Seat.findById(req.params.seatId)
+  if (!seat) {
+    res.status(400)
+    throw new Error('Seat not found')
+  }
   try {
-    const passengers = await Passenger.deleteMany()
+    const updatedSeat = await Seat.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    })
+    res.status(201).json(updatedSeat)
+  } catch (error) {
+    res.status(400).json({ message: error })
+  }
+})
+
+// DELETE SEATS ROUTE
+admin.delete('/delete-seats/:seatId', async (req, res) => {
+  const seat = await Seat.findById(req.params.seatId)
+  if (!seat) {
+    res.status(400)
+    throw new Error('Seat not found')
+  }
+  try {
+    await seat.deleteOne()
+    res.status(201).json({ id: req.params.seatId })
+  } catch {
+    res.status(400).json({ message: error })
+  }
+})
+
+admin.delete('/delete-passenger/:passengerId', async (req, res) => {
+  const passengers = await Passenger.findById(res.params.passengerId)
+  if (!passengers) {
+    res.status(400)
+    throw new Error('Passenger not found')
+  }
+  try {
+    await passengers.deleteOne()
     res.status(200).json(passengers)
   } catch (error) {
     res.status(500).json({
@@ -184,7 +205,6 @@ admin.delete('/passengers', async (req, res) => {
   }
 })
 
-// REMOVE AFTER TESTING !!!!!!!!!!!
 admin.delete('/delete-reservation/:reservationId', async (req, res) => {
   try {
     const reservation = await Reservation.findByIdAndDelete(
